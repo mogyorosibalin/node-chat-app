eda3666ed272803751113f1fefa06a7118dfb17f
Balint Mogyorosi <mogyorosibalin@gmail.com>
2019-02-13 14:35:35 +0100

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 const socket = io();
 
+function scrollToBottom() {
+    // Selectors
+    const messages = $('#messages');
+    const newMessage = messages.children('li:last-child');
+    // Heights
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connected to server');
 });
@@ -13,6 +29,7 @@ socket.on('newMessage', function(message) {
         text: message.text
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -24,6 +41,7 @@ socket.on('newLocationMessage', function(message) {
         url: message.url
     });
     $('#messages').append(html);
+    scrollToBottom()
 });
 
 socket.on('disconnect', function() {
