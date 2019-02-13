47639714f69c34f670b7f238b5421a5ccbde1236
Balint Mogyorosi <mogyorosibalin@gmail.com>
2019-02-13 10:40:45 +0100

    Emit new message on create message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ const socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'George',
-        text: 'Hey, Everything fine man!'
-    });
 });
 
 socket.on('newMessage', function(message) {
