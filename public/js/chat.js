1968abf79280e4e5aabffe1e543b353e6c2ad333
Balint Mogyorosi <mogyorosibalin@gmail.com>
2019-02-14 11:07:01 +0100

    Send messages to only people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -69,7 +69,6 @@ $('#message-form').on('submit', function(event) {
     event.preventDefault();
     const messageTextbox = $('[name=message]');
     socket.emit('createMessage', {
-        from: 'User',
         text: messageTextbox.val()
     }, function() {
         messageTextbox.val('');
